9c236c32798cd53d669b1664a4f71815be5963be
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-05-10 10:21:51 +0900

    Update Changes and add warnings

--- lib/SPVM/IO/File.pm
+++ lib/SPVM/IO/File.pm
@@ -6,12 +6,14 @@ our $VERSION = '0.01';
 
 =head1 NAME
 
-SPVM::IO::File - File handle
+SPVM::IO::File - File Input/Output
 
 =head1 SYNOPSYS
   
-  use SPVM::IO::File;
+  use IO::File;
 
 =head1 DESCRIPTION
 
 L<SPVM::IO::File> provides file input/output stream.
+
+B<SPVM::IO::File is now development release. The changes without warnings are offten quit will be done.>
